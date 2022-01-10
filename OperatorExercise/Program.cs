aee616cdae975bfb915ef945fc6501413f508047
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-10 13:29:19 -0600

    message

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+int a = 17;
+int b = 4;
+
+int quotient = a / b;
+int remainder = a % b;
+
+if(a== 17 && b==4)
+
+17/4 is 4 remainder 1
