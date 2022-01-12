62254f48e1e17ba9eddeaf1a387718dfd23ca8e2
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-12 17:27:55 -0600

    message

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,24 +4,69 @@ namespace OperatorExercise
 {
     class Program
     {
+        private static object quoutient;
 
         static void Main(string[] args)
         {
-            double johnsVariable = 1.1;
+
 
 
             int a = 17;
             int b = 4;
-            var quotient = a / b;
-            var remainder = a % b;
 
-            if (a == 17 && b == 4)
-            {
-                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder})"); 
-            }                
-             
+            //Addition
 
+            int answer = a + b;
+
+            Console.WriteLine($"Addition:{answer}");
+
+
+            //Subtraction
+
+            int answer2 = a - b;
+
+            Console.WriteLine($"Subtraction: {answer2}");
+
+
+            //Multipication
+
+            int answer3 = a * b;
+
+            Console.WriteLine($"Multiplication:{answer3}");
+
+
+
+            //Division
+
+            int answer4 = a / b;
+            var remainder = a % b; //17 / 4 = R1
+
+            
+
+
+            Console.WriteLine($"The quotient is {answer4} with a remainder of {remainder}");
+
+            Console.WriteLine();
+
+            AreaOfCircle();
+            
+            
+         }
+
+
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("what is the radius of your circle ?");
+            var radius = double.Parse(Console.ReadLine());
+
+            var area = Math.PI * radius * radius;
+
+            Console.WriteLine(area);
+
+           
 
         }
+
     }
+
 }
