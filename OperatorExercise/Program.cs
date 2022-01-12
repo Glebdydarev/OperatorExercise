cea0eef0b00900c094ba033ef027ed6f8806a7be
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-12 08:23:16 -0600

    message

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,18 +4,24 @@ namespace OperatorExercise
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double johnsVariable = 1.1;
+
+
+            int a = 17;
+            int b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder})"); 
+            }                
+             
+
+
         }
     }
 }
-int a = 17;
-int b = 4;
-
-int quotient = a / b;
-int remainder = a % b;
-
-if(a== 17 && b==4)
-
-17/4 is 4 remainder 1
